eb2098466704e6f8e0de8ddc658ddcf20f758074
camiloco345 <camilo.enrique.ovalle@gmail.com>
2021-09-29 19:10:10 -0500

    tengo un error en la linea 12 hay que revisarlo

--- Callbacks/Challenge.js
+++ Callbacks/Challenge.js
@@ -1,4 +1,5 @@
 let XMLHttprequest = require('xmlhttprequest').XMLHttpRequest;
+let API = 'https://rickandmortyapi.com/api/character/'
 
 let fetchData = (url_api, callBack)=>{
     let xhttp = new XMLHttprequest()
@@ -8,10 +9,23 @@ let fetchData = (url_api, callBack)=>{
             if(xhttp.status === 200){
                 callBack(null, JSON.parse(xhttp.responseText))
             }else{
-                const Error = new Error('Error' + url_api)
-                return callBack(Error, null)
+                const error = new error('error' + url_api)
+                return callBack(error, null)
             }
         }
     }
     xhttp.send()
 }
+
+fetchData(API,function(error1, data1){
+    if(error1) return console.error(error1)
+        fetchData(API + data1.results[0].id, function(error2, data2){
+            if(error2) return console.error(error2)
+                fetchData(API + data2.origin.url, function(error3, data3){
+                    if(error3) return console.error(error3)
+                    console.log(data1.info.count)
+                    console.log(data2.name)
+                    console.log(data3.dimension)
+                })
+        })
+})
