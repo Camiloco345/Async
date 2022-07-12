6e27165120cef058bb291f480b55242585a70652
Camilo Ovalle <camilo.erique.ovalleqgmail.com>
2022-07-11 23:20:12 -0500

    Practicando lo mas posible con las promesas para entender bien su funcionamineto

--- src/Promises/PromisesChallenges.js
+++ src/Promises/PromisesChallenges.js
@@ -1,12 +1,33 @@
 import fetch from "node-fetch";
 const API = 'https://pokeapi.co/api/v2'
 
-fetch(`${API}/pokemon`)
-.then(response => response.json())
-.then(async data => {
-        const bulbasaur = data.results[0] 
-         const response = await fetch(`${API}/pokemon/${bulbasaur.name}`);
-        const data_1 = await response.json();
-        return console.log(data_1.abilities);
-        
+let fetchData = (urlApi)=>{
+    return fetch(urlApi)
+};
+//This is the first API calling to fetch a pokemon list
+ fetchData(`${API}/pokemon`)
+.then( response => response.json() )
+.then(data => {
+    const pokemon = data.results[0]
+    console.log(`Name: ${pokemon.name}`)
+    //This is a new addition to the url using the information we fetched from the API 
+   return fetchData(`${API}/pokemon/${pokemon.name}`)
+    .then(response => response.json() )
+    .then(data2 => {
+       const abilities = data2.abilities
+       //here we accede to the object information and extract the ability information
+       const ability = abilities[1]
+       console.log(`Ability name: ${ability.ability.name}`)
+       const abilityAPI = ability.ability.url
+       //here we fetch the url to access the abilities description
+        return fetchData(abilityAPI)
+        .then(response => response.json())
+        //here we finally get the ability information and description we want
+        .then(description => {
+            const effect = description.effect_entries[1].effect
+            console.log(`Effect name: ${effect}`)
+        })
     })
+})
+
+
